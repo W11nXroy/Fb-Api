def97141868f500d80cc5873edf58a48099cf167
W11nXroy <114358677+W11nXroy@users.noreply.github.com>
2022-09-27 22:44:15 +0500

    Update Api.py

--- Api.py
+++ Api.py
@@ -5,7 +5,7 @@ os.system('git pull')
 print('<------------------------------------>')
 bit = platform.architecture()[0]
 if bit=='64bit':
-    print('[•] Congratulations your device support this tool)
+    print('[•] Congratulations your device support this tool')
     import s
 else:
     print('\033[1;31m[×] Sorry Device Not Support')
