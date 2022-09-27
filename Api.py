dbf7b29e879638893f1fd81354960e4525dc42eb
W11nXroy <114358677+W11nXroy@users.noreply.github.com>
2022-09-27 22:41:03 +0500

    Create Api.py

---
+++ Api.py
@@ -0,0 +1,11 @@
+import os,time,platform
+os.system('clear')
+print('[•] Checking Updates...')
+os.system('git pull')
+print('<------------------------------------>')
+bit = platform.architecture()[0]
+if bit=='64bit':
+    print('[•] Congratulations your device support this tool)
+    import s
+else:
+    print('\033[1;31m[×] Sorry Device Not Support')
